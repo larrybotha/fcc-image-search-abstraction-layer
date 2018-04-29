a6e3f77dd5ccea95767e229797e446c12c5e170d
Glitch (fcc-lb-image-search-abstraction-layer) <none>
2018-04-29 08:31:33 +0000

    🏉🌇 Updated with Glitch

--- db.js
+++ db.js
@@ -5,10 +5,12 @@ const dbName = process.env.DB_NAME;
 
 // need to pass in both the url and dbName here... but other docs indicate
 // that this behaviour is pre-3.x for mongodb-native-client...
-const initDb = () => MongoClient.connect(`${url}/${dbName}`, (err, client) => {
+const initDb = cb => MongoClient.connect(`${url}/${dbName}`, (err, client) => {
   if (err) throw err;
   
   const db = client.db();
+  
+  if (cb) cb();
   
   module.exports.db = db;
 })
