991be750560ac212fc5d5f63c01e2f362812caf4
Glitch (fcc-lb-image-search-abstraction-layer) <none>
2018-04-29 07:11:52 +0000

    👝💳 Updated with Glitch

--- api.js
+++ api.js
@@ -4,8 +4,26 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const GOOGLE_CSE_URL = "https://www.googleapis.com/customsearch/v1";
+const GOOGLE_CSE_API_KEY = process.env.GOOGLE_CSE_API_KEY;
+const GOOGLE_CSE_CX = process.env.GOOGLE_CSE_CX;
+
+const getRequestUrl = (q, page = 0) => {
+  const numResults = 10;
+  const params = {
+    cx: GOOGLE_CSE_CX,
+    key: GOOGLE_CSE_API_KEY,
+    num: numResults,
+    start: numResults * page,
+  };
+  
+  return Object.keys(params).reduce((acc, k) => `${acc}&${k}=${params[k]}`, '');
+};
+
 apiRouter.get('/imagesearch/:search', (req, res) => {
-  res.json({status: 200});
+  const requestUrl = getRequestUrl(req.params.search, req.query.offset);
+  
+  res.json({req: requestUrl});
 })
 
 apiRouter.get('/latest/imagesearch/', (req, res) => {
